c9b335966ad64d69b02d66d6cdd7b442121209f0
tristanrichard <tristangrichard@gmail.com>
2013-11-05 15:05:39 +0100

    MJMethodCallExpr

--- MiniJava_A5/src/compiler/IR/MJMethodCallExpr.java
+++ MiniJava_A5/src/compiler/IR/MJMethodCallExpr.java
@@ -16,7 +16,7 @@ public class MJMethodCallExpr extends MJExpression {
 	private LinkedList<MJExpression> arglist;
 
 	private MJMethod target;
-	
+
 	public MJMethodCallExpr(MJIdentifier m, LinkedList<MJExpression> arglist) {
 		this.method = m;
 		this.arglist = arglist;
@@ -39,18 +39,43 @@ public class MJMethodCallExpr extends MJExpression {
 		}
 		prepri.print(")");
 	}
-	
+
 	MJType typeCheck() throws TypeCheckerException {
-		
-		// here you should enter the code to type check this class
-		
-		
-		return MJType.getVoidType();
+
+		MJClass decl = null;
+		MJClass cla = IR.currentClass;
+		MJIdentifier ident = this.method;
+
+		if(ident instanceof MJSelector){
+
+			MJSelector sel = (MJSelector) ident;
+			MJType type = sel.getObject().typeCheck();
+
+			if(ident.getType().isClass()){
+				decl = sel.getObject().typeCheck().getDecl();
+				cla = type.getDecl();
+				ident = sel.getField();
+			}	
+		}
+
+		for(MJExpression args : arglist)
+		{
+			args.typeCheck();
+		}
+
+		try {
+			IR.classes.lookupMethod(decl, cla.getName(), arglist);
+		} catch (ClassErrorMethod e) {
+			throw new TypeCheckerException(e.getMessage());
+		} catch (MethodNotFound e) {
+			throw new TypeCheckerException(e.getMessage());
+		}
+		return this.type;
 	}
 
 	void variableInit(HashSet<MJVariable> initialized)
 			throws TypeCheckerException {
-		
+
 		// here you should enter the code to check whether all variables are initialized
 	}
 
